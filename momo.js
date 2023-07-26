c226c1175de239ed79073f49a3b9de44abda57b1
Momo Yang <momo.yang666@gmail.com>
2023-07-26 16:29:36 +0200

    users login function

--- momo.js
+++ momo.js
@@ -88,6 +88,38 @@ const slider = function () {
 
 slider();
 
+/////////////////////////////
+// Modal window
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnOpenModal = document.querySelector(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModal);
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
 let url = "https://my-json-server.typicode.com/mrkiley/cwb2023-onlineshop/db";
 
 const productsBox = document.querySelector(".products");
@@ -102,9 +134,8 @@ const getData = async (url) => {
 
 ////////////////////////////////////////
 
-async function setUpElementsForData(data) {
+async function setUpProductsCardForData(data) {
   const products = data.products;
-
   const rootVegesContainer = document.querySelector(".rootvege-products");
   const leafygreensContainer = document.querySelector(".leafygreens-products");
   const otherVegesContainer = document.querySelector(".othervege-products");
@@ -138,7 +169,6 @@ async function setUpElementsForData(data) {
     "Celery",
     "Leek",
   ];
-  // const productNames = products.map((product) => product.name);
 
   //Tabbled Components
 
@@ -172,54 +202,88 @@ async function setUpElementsForData(data) {
           </div>
           <div class="shopping--btns">
             <button class ="btn btn-heart">
-            <img class="heart-img product-like-btn" src="images/favorite.png" alt="heart image">
+              <img class="heart-img product-like-btn" src="images/favorite.png" alt="heart image">
             </button>
-            <button class ="btn btn-addtocart">
-            <img class="cart-img product-cart-btn" src="images/shopping-cart.png" alt="shopping cart image">
-            </button>
+
+            
+              <button class ="btn btn-addtocart cart-container" id="cart-button" >
+              <img class="cart-img product-cart-btn" src="images/shopping-cart.png" alt="shopping cart image">
+              </button>
+            
           </div>
         </div>`;
 
     return html;
   };
 
-  //Category--root Veges
-  const rootVegesProducts = products.filter((product) =>
-    rootVeges.includes(product.name)
-  );
+  const categorizeProducts = function () {
+    //Category--root Veges
+    const rootVegesProducts = products.filter((product) =>
+      rootVeges.includes(product.name)
+    );
 
-  rootVegesProducts.forEach((vege) =>
-    rootVegesContainer.insertAdjacentHTML("afterbegin", productCard(vege))
-  );
+    rootVegesProducts.forEach((vege) =>
+      rootVegesContainer.insertAdjacentHTML("afterbegin", productCard(vege))
+    );
 
-  //Category--Leafy Greens
-  const leafyGreensProducts = products.filter((product) =>
-    leafyGreens.includes(product.name)
-  );
+    //Category--Leafy Greens
+    const leafyGreensProducts = products.filter((product) =>
+      leafyGreens.includes(product.name)
+    );
 
-  leafyGreensProducts.forEach((greens) =>
-    leafygreensContainer.insertAdjacentHTML("afterbegin", productCard(greens))
-  );
+    leafyGreensProducts.forEach((greens) =>
+      leafygreensContainer.insertAdjacentHTML("afterbegin", productCard(greens))
+    );
 
-  //Category--Other Vegetables
+    //Category--Other Vegetables
 
-  const otherVegeProducts = products.filter((product) =>
-    otherVeges.includes(product.name)
-  );
+    const otherVegeProducts = products.filter((product) =>
+      otherVeges.includes(product.name)
+    );
 
-  otherVegeProducts.forEach((other) =>
-    otherVegesContainer.insertAdjacentHTML("afterbegin", productCard(other))
-  );
+    otherVegeProducts.forEach((other) =>
+      otherVegesContainer.insertAdjacentHTML("afterbegin", productCard(other))
+    );
+  };
 
+  categorizeProducts();
   // console.log(products, users);;
+}
+
+async function usersLog(data) {
+  const inputFirstName = document.querySelector(".input__first--name");
+  const inputLastName = document.querySelector(".input__last--name");
+  const inputGender = document.querySelector(".form__input--gender");
+  const btnLogIn = document.getElementById("btn-login");
+  const btnText = document.querySelector(".login-text");
+
+  const users = data.users;
+
+  btnLogIn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let currentUser = users.find(
+      (user) =>
+        user.familyName.toLowerCase() === inputLastName.value.toLowerCase() &&
+        user.givenName.toLowerCase() === inputFirstName.value.toLowerCase() &&
+        user.gender === inputGender.value
+    );
+
+    if (!currentUser) return;
+
+    console.log(currentUser);
+
+    closeModal();
+    btnText.textContent = `Welcome Back, ${currentUser.givenName}`;
+    btnText.style.borderRadius = "5px";
+  });
 }
 
 const setUpPage = async () => {
   const data = await getData(url);
-  await setUpElementsForData(data);
-  await setCart(data);
+  await setUpProductsCardForData(data);
+  await usersLog(data);
 
   return data;
 };
 
-setUpPage().then((data) => console.log(data.users));
+setUpPage().then();
